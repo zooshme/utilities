7042c89a5dab90c8d14c2877240e3680d5f7a5ea
Ovidiu Spatacian-Tarnu <contact@zoosh.me>
2015-05-03 23:14:19 +0100

    added typekit utility

---
+++ typekit.js
@@ -0,0 +1,20 @@
+var typekit = function(kidId) {
+  var config = {
+    kitId: kidId
+  };
+  var d = false;
+  var tk = document.createElement('script');
+  tk.src = '//use.typekit.net/' + config.kitId + '.js';
+  tk.type = 'text/javascript';
+  tk.async = 'true';
+  tk.onload = tk.onreadystatechange = function() {
+    var rs = this.readyState;
+    if (d || rs && rs != 'complete' && rs != 'loaded') return;
+    d = true;
+    try { Typekit.load(config); } catch (e) {}
+  };
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(tk, s);
+};
+
+modul.exports = typekit;
